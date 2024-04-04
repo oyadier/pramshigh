cb87be167515621bac1c9911df4451a97be9ae33
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-04-04 01:15:33 +0000

    Web dynmic

---
+++ web_dynamic/flask_app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+import os
+app = Flask(__name__)
+
+@app.route("/home/")
+def pramshigh():
+    #    main app leading to the homepage of the app
+       return render_template('index.html')
+    
+
+@app.route("/home/fisher/")
+@app.route("/fisher/")
+def fisher():
+      return render_template('fisher.html')
+
+@app.route("/courses/")
+def courses():
+     return render_template('courses.html')
+
+@app.route("/base/")
+def base_url():
+     return render_template('base.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
